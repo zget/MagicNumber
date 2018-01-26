013dbe31e09286d84f10d378b6a63ac14bb72d80
zget <dgetahun84@gmail.com>
2018-01-26 11:43:52 -0500

    magic number pattern

---
+++ src/main/java/week1challenge2/demo/MainController.java
@@ -0,0 +1,40 @@
+package week1challenge2.demo;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/getMagicNumber")
+
+    public String magicPattern(@RequestParam("myNumber") int myNumber)
+    {
+
+
+        String number_list=" ";
+        do
+            {
+                if(myNumber%2==0)
+                {
+                    number_list=number_list+Integer.toString(myNumber)+"  ";
+                    myNumber/=2;
+                }
+
+                else
+                {
+                    number_list=number_list+Integer.toString(myNumber)+"  ";
+                    myNumber=myNumber*3+1;
+
+                }
+            }
+        while(myNumber>1);
+
+
+        return number_list+" "+Integer.toString(1);
+    }
+
+
+}
---
+++ src/test/java/week1challenge2/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package week1challenge2.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
